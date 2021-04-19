958e625c16d28783f74e6732bef24212f7ab3ecd
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-04-19 08:15:23 +0700

    Bahan Ajar

---
+++ lib/pages/home_provider.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomeProvider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GET - STATEFUL"),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              child: Image.network("https://reqres.in/img/faces/2-image.jpg"),
+            ),
+            SizedBox(height: 20),
+            FittedBox(
+              child: Text(
+                "ID : Belum ada data",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
+            FittedBox(
+              child: Text(
+                "Belum ada data",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Email : ", style: TextStyle(fontSize: 20))),
+            FittedBox(
+              child: Text(
+                "Belum ada data",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text(
+                "GET DATA",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_stateful.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GET - STATEFUL"),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              child: Image.network("https://reqres.in/img/faces/2-image.jpg"),
+            ),
+            SizedBox(height: 20),
+            FittedBox(
+              child: Text(
+                "ID : Belum ada data",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
+            FittedBox(
+              child: Text(
+                "Belum ada data",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            FittedBox(child: Text("Email : ", style: TextStyle(fontSize: 20))),
+            FittedBox(
+              child: Text(
+                "Belum ada data",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text(
+                "GET DATA",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
